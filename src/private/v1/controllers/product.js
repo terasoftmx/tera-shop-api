6e41baf12f1da2a13e611a14180f56d83f19bdab
cris.medinill@gmail.com <cris.medinill@gmail.com>
2020-02-01 17:23:48 -0600

    refactor(api): Refactor product endpoint

--- src/private/v1/controllers/product.js
+++ src/private/v1/controllers/product.js
@@ -4,30 +4,12 @@ const { Product } = require('../../../utils/index')
 exports.getProducts = (req, res) => {
   mysql.getProducts().then((response) => {
     const products = []
-    response.forEach(rowRaw => {
-      const product = new Product(
-        rowRaw.ID_CART,
-        rowRaw.CART,
-        rowRaw.NART,
-        rowRaw.CATEGORIA,
-        rowRaw.MARCA,
-        rowRaw.MODELO,
-        rowRaw.COLOR,
-        rowRaw.MEDIDAS,
-        rowRaw.PRECIOA,
-        rowRaw.PRECIOB,
-        rowRaw.PRECIOC,
-        rowRaw.PRECIOD,
-        rowRaw.PRECIOE,
-        rowRaw.PRECIOF,
-        rowRaw.EXTENCION,
-        rowRaw.PROV,
-        rowRaw.FAB,
-        rowRaw.PROMO,
-        rowRaw.PMES,
-      )
-      products.push(product)
-    })
+    const dataResponse = _transformDbResponseToProductObject(response)
+    if (dataResponse) {
+      res.status(200).json({data: dataResponse})
+    } else {
+      res.status(404).json({data: 'not found'})
+    }
     res.status(200).json({data: products})
   }).catch(e => res.status(500).json({ error: e.message }))
 }
@@ -35,32 +17,56 @@ exports.getProducts = (req, res) => {
 exports.getProduct = (req, res) => {
   const productId = req.params.productId
   mysql.getProductById(productId).then((response) => {
-    if (rowRaw.length === 0) {
-      res.status(404).json({data: 'product not found'})
+    const dataResponse = _transformDbResponseToProductObject(response)
+    if (dataResponse) {
+      res.status(200).json({data: dataResponse})
+    } else {
+      res.status(404).json({data: 'not found'})
     }
-
-    const rowRaw = response[0]
-    const product = new Product(
-      rowRaw.ID_CART,
-      rowRaw.CART,
-      rowRaw.NART,
-      rowRaw.CATEGORIA,
-      rowRaw.MARCA,
-      rowRaw.MODELO,
-      rowRaw.COLOR,
-      rowRaw.MEDIDAS,
-      rowRaw.PRECIOA,
-      rowRaw.PRECIOB,
-      rowRaw.PRECIOC,
-      rowRaw.PRECIOD,
-      rowRaw.PRECIOE,
-      rowRaw.PRECIOF,
-      rowRaw.EXTENCION,
-      rowRaw.PROV,
-      rowRaw.FAB,
-      rowRaw.PROMO,
-      rowRaw.PMES,
-    )
-    res.status(200).json({data: product})
   }).catch(e => res.status(500).json({ error: e.message }))
+}
+
+const _transformDbResponseToProductObject = dbResponse => {
+  if (dbResponse.length === 0) {
+    return false
+  }
+
+  if (dbResponse.length === 1) {
+    const rowRaw = dbResponse[0]
+
+    return _createProductObject(rowRaw)
+  }
+
+  if (dbResponse.length > 1) {
+    const products = []
+    dbResponse.forEach(rowRaw => {
+      products.push(_createProductObject(rowRaw))
+    })
+
+    return products
+  }
+}
+
+const _createProductObject = rowRaw => {
+  return new Product(
+    rowRaw.ID_CART,
+    rowRaw.CART,
+    rowRaw.NART,
+    rowRaw.CATEGORIA,
+    rowRaw.MARCA,
+    rowRaw.MODELO,
+    rowRaw.COLOR,
+    rowRaw.MEDIDAS,
+    rowRaw.PRECIOA,
+    rowRaw.PRECIOB,
+    rowRaw.PRECIOC,
+    rowRaw.PRECIOD,
+    rowRaw.PRECIOE,
+    rowRaw.PRECIOF,
+    rowRaw.EXTENCION,
+    rowRaw.PROV,
+    rowRaw.FAB,
+    rowRaw.PROMO,
+    rowRaw.PMES,
+  )
 }
